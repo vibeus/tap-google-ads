8dca1e2dafa3cbfb53bfa3b1066dd34954267c3f
LesleyDing <leslietdhz2014@gmail.com>
2022-11-03 14:24:00 +0800

    UPDATE add conversion metrics streams

--- tap_google_ads/streams/__init__.py
+++ tap_google_ads/streams/__init__.py
@@ -1,7 +1,9 @@
 from .campaigns import Campaigns
 from .campaign_metrics import CampaignMetrics
+from .campaign_metrics_conversions import CampaignMetricsConversions
 from .ad_groups import AdGroups
 from .ad_group_metrics import AdGroupMetrics
+from .ad_group_metrics_conversions import AdGroupMetricsConversions
 
 
 def create_stream(stream_id):
@@ -9,9 +11,13 @@ def create_stream(stream_id):
         return Campaigns()
     elif stream_id == "campaign_metrics":
         return CampaignMetrics()
+    elif stream_id == "campaign_metrics_conversions":
+        return CampaignMetricsConversions()
     elif stream_id == "ad_groups":
         return AdGroups()
     elif stream_id == "ad_group_metrics":
         return AdGroupMetrics()
+    elif stream_id == "ad_group_metrics_conversions":
+        return AdGroupMetricsConversions()
 
     assert False, f"Unsupported stream: {stream_id}"
